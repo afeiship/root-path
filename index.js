b024efe06d755f52efe4f5e8314a8b34a6539d66
feizheng <1290657123@qq.com>
2016-12-10 21:58:48 +0800

    add limit bug

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var path = require('path');
 var fs = require('fs');
+var count = 0,
+  limit = 10;
 
 
 function discoverRoot(inRootPoint) {
-  var rootPoint =  inRootPoint || '.git';
-  var dirs=fs.readdirSync('.');
-  if(dirs.indexOf(rootPoint)==-1){
-    process.chdir(path.resolve(__dirname,'..'))
-    return discoverRoot(rootPoint);
+  var rootPoint = inRootPoint || '.git';
+  var dirs = fs.readdirSync('.');
+  if (dirs.indexOf(rootPoint) == -1) {
+    process.chdir('..');
+    count++;
+    return count < limit ? discoverRoot(rootPoint) : path.resolve('.');
   }
-  return path.resolve(__dirname,'.');
+  return path.resolve(__dirname, '.');
 }
 
 module.exports = discoverRoot;

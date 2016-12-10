b2c0b90bd732b1cd42ee994a1e98f5df99df45fd
feizheng <1290657123@qq.com>
2016-12-10 21:39:51 +0800

    bugfix

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 var path = require('path');
-module.exports = path.dirname(require.main.filename);
+var fs = require('fs');
+
+
+function discoverRoot(inRootPoint) {
+  var rootPoint =  inRootPoint || '.git';
+  var dirs=fs.readdirSync('.');
+  if(dirs.indexOf(rootPoint)==-1){
+    return discoverRoot(path.resolve(__dirname,'..'));
+  }
+  return path.resolve(__dirname,'.');
+}
+
+module.exports = discoverRoot;
